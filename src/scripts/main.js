58b193ffdb6fd0637641b33f539a38545dd6596c
LakziaCarlosChaves <lalacarloschaves@gmail.com>
2024-09-12 11:57:28 -0300

    criando a contagem regressiva

---
+++ src/scripts/main.js
@@ -0,0 +1,34 @@
+AOS.init();
+
+
+const dataDoEvento = new Date("Sep 22, 2024 15:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs= 1000 * 60 * 60 * 24;
+    const horaEmMs= 1000 * 60 * 60;
+    const minutoEmMs= 1000 * 60;
+
+    const diasAteOEvento =  Math.floor (distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento% minutoEmMs) / 1000);
+
+    console.log(diasAteOEvento);
+    console.log(horasAteOEvento);
+    console.log(minutosAteEvento);
+    console.log(segundosAteOEvento);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteEvento}m ${segundosAteOEvento}s`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = `Evento expirado`;
+    }
+
+}, 1000) 
